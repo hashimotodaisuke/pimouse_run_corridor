c7cbbeead0a12b3c9cf8a29bef8fa59b8dca6391
Daisuke Hashimoto <hashimoto@example.com>
2017-05-22 04:16:08 +0000

    add and modify launch

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,7 +1,7 @@
 #!/bin/bash -xve
 
 #sync and make
-rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor
+rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
 cd ~/catkin_ws/src
 git clone --depth=1 https://github.com/hashimotodaisuke/pimouse_ros.git
 

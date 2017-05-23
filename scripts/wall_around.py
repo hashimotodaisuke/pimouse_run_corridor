aa09e2876900a6659cbf07219f3db1edbacdc087
Daisuke Hashimoto <hashimoto@example.com>
2017-05-23 08:12:40 +0000

    add wall around

--- scripts/wall_around.py
+++ scripts/wall_around.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import rospy, copy
+import math
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.msg import LightSensorValues
 
-class WallStopAccel():
+DISTANCE_VALUE = 50
+
+class WallAround():
 	def __init__(self):
 		# Active "cmd_vel" publisher to broadcast motor frequency
 		self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
@@ -18,26 +21,41 @@ class WallStopAccel():
 	def callback(self, message):
 		self.sensor_values = message
 
+	def wall_front(self, ls):
+		return ls.left_forward > 50 or ls.right_forward > 50
+
+	def too_right(self, ls):
+		return ls.right_side > 50
+
+	def too_left(self, ls):
+		return ls.left_side > 50
+
 	def run(self):
-		rate = rospy.Rate(10)
+		# wake up every 20Hz(50ms)and check lightsensor
+		rate = rospy.Rate(20)
 		data = Twist()
 
-		accel = 0.02
-		data.linear.x = 0.0
-
+		data.linear.x = 0.3
+		data.angular.z = 0.0
+		
 		while not rospy.is_shutdown():
-			data.linear.x += accel
-	
-			if   self.sensor_values.sum_all >= 200: data.linear.x = 0.0
-			elif data.linear.x <= 0.2:              data.linear.x = 0.2
-			elif data.linear.x >= 0.8:              data.linear.x = 0.8    
+			if self.wall_front(self.sensor_values):
+				data.angular.z = - math.pi
+			elif self.too_right(self.sensor_values):
+				data.angular.z = math.pi
+			elif self.too_left(self.sensor_values):
+				data.angular.z = - math.pi
+			else:
+				error = DISTANCE_VALUE - self.sensor_values.left_side
+				data.angular.z = error * math.pi / 180.0
 			
+			# publish for travis test
 			self.cmd_vel.publish(data)
 			rate.sleep()
 
 if __name__ == '__main__':
-	rospy.init_node('wall_stop_accel')
-	
+	rospy.init_node('wall_around')
+	# rospy.init_node('wall_around', log_level=rospy.DEBUG)
 	# Motor class@pimouse_ros provid /motor_on and /motor_off service
 	rospy.wait_for_service('/motor_on')  # wait for motor on service wake up
 	rospy.wait_for_service('/motor_off') # wait for motor off service wake up
@@ -52,6 +70,6 @@ if __name__ == '__main__':
 	rospy.ServiceProxy('/motor_on', Trigger).call()
 
 	#run until terminate
-	WallStopAccel().run()
+	WallAround().run()
 
 
--- scripts/wall_trace.py
+++ scripts/wall_trace.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import rospy, copy
+import math
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.msg import LightSensorValues
 
-class WallStopAccel():
+DISTANCE_VALUE = 50
+
+class WallTrace():
 	def __init__(self):
 		# Active "cmd_vel" publisher to broadcast motor frequency
 		self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
@@ -19,24 +22,45 @@ class WallStopAccel():
 		self.sensor_values = message
 
 	def run(self):
-		rate = rospy.Rate(10)
+		# wake up every 20Hz(50ms)and check lightsensor
+		rate = rospy.Rate(20)
 		data = Twist()
 
 		accel = 0.02
-		data.linear.x = 0.0
 
 		while not rospy.is_shutdown():
+			s = self.sensor_values
 			data.linear.x += accel
 	
-			if   self.sensor_values.sum_all >= 200: data.linear.x = 0.0
-			elif data.linear.x <= 0.2:              data.linear.x = 0.2
-			elif data.linear.x >= 0.8:              data.linear.x = 0.8    
+			if   s.sum_forward >= 50: 
+				data.linear.x = 0.0
+				rospy.loginfo("stop by det front wall="+str(s.sum_forward))
+			elif data.linear.x <= 0.2:data.linear.x = 0.2
+			elif data.linear.x >= 0.8:data.linear.x = 0.8 
+			   
+			# pimouse run in only left side
+
+			# pimouse should not run around when it stops
+			if data.linear.x < 0.2:   data.angular.z = 0.0
+			# pimouse should not turn left when there is no wall
+			elif s.left_side < 10:    data.angular.z = 0.0
+			else:
+				# target means the distance between wall and pimouse is 1cm
+				# this value is decided in examination
+				target = DISTANCE_VALUE
+				# if s.left_side is +150(error=-2), the distance is 2cm(near)
+				# if s.left_side is -150(error=+2), the distance is 2cm(far)
+				error = (target - s.left_side)/50.0
+				# set 3 degree/distance(cm) with radian change
+				data.angular.z = error*3*math.pi / 180.0
+				rospy.loginfo("err="+str(error)+",angl="+str(data.angular))
 			
+			# publish for travis test
 			self.cmd_vel.publish(data)
 			rate.sleep()
 
 if __name__ == '__main__':
-	rospy.init_node('wall_stop_accel')
+	rospy.init_node('wall_trace')
 	
 	# Motor class@pimouse_ros provid /motor_on and /motor_off service
 	rospy.wait_for_service('/motor_on')  # wait for motor on service wake up
@@ -52,6 +76,6 @@ if __name__ == '__main__':
 	rospy.ServiceProxy('/motor_on', Trigger).call()
 
 	#run until terminate
-	WallStopAccel().run()
+	WallTrace().run()
 
 

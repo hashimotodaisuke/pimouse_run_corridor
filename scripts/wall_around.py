d5abd2b6239ab7a1c48f3376852ef194f36482cd
Daisuke Hashimoto <hashimoto@example.com>
2017-05-30 05:43:36 +0000

    with reptrino

--- scripts/wall_around.py
+++ scripts/wall_around.py
@@ -3,38 +3,65 @@ import rospy, copy
 import math
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
-from pimouse_ros.msg import LightSensorValues
+# from pimouse_ros.msg import LightSensorValues
+from geometry_msgs.msg import WrenchStamped
+#WrenchStamped structure is
+#Header header
+	#uint32 seq
+	#time stamp
+	#string frame_id
+#Vector3 force
+	#float64 x
+	#float64 y
+	#float64 z
+#Vector3 torque
+	#float64 x
+	#float64 y
+	#float64 z
 
-DISTANCE_VALUE = 50
-THRESHOLD_FRONT_WALL = 50
-THRESHOLD_LEFT_WALL = 50
-THRESHOLD_RIGHT_WALL = 50
+DISTANCE_VALUE = 5
+THRESHOLD_FRONT_WALL = 5
+THRESHOLD_LEFT_WALL = 5
+THRESHOLD_RIGHT_WALL = 5
 
 class WallAround():
 	def __init__(self):
 		# Active "cmd_vel" publisher to broadcast motor frequency
 		self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
-		# Get initail lightsensor values
-		self.sensor_values = LightSensorValues()
+		# set force sensor initail values
+		self.sensor = WrenchStamped()
 
-		# Active lightsensor subscriver to ger rightsensor values
-		rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
+		self.force_x = 0
+		self.force_y = 0
+		self.force_z = 0
+
+		# Active force_torque  subscriver to ger force sensor values
+		rospy.Subscriber('/leptrino_force_torque/force_torque', WrenchStamped, self.callback)
 
 	def callback(self, message):
-		self.sensor_values = message
+		self.sensor = message
+		if self.sensor.wrench.force.x < 0:
+			self.sensor.wrench.force.x = -1 * self.sensor.wrench.force.x
+		self.force_x = int(self.sensor.wrench.force.x)
+		if self.sensor.wrench.force.y < 0:
+			self.sensor.wrench.force.y = -1 * self.sensor.wrench.force.y
+		self.force_y = int(self.sensor.wrench.force.y)
+		if self.sensor.wrench.force.z < 0:
+			self.sensor.wrench.force.z = -1 * self.sensor.wrench.force.z
+		self.force_z = int(self.sensor.wrench.force.z)
 
-	def wall_front(self, ls):
-		return ls.left_forward > THRESHOLD_FRONT_WALL  or ls.right_forward > THRESHOLD_FRONT_WALL
+	def wall_front(self):
+		return self.force_x > THRESHOLD_FRONT_WALL  or self.force_y > THRESHOLD_FRONT_WALL
 
-	def too_right(self, ls):
-		return ls.right_side > THRESHOLD_RIGHT_WALL
+	def too_right(self):
+		return self.force_x > THRESHOLD_RIGHT_WALL
 
-	def too_left(self, ls):
-		return ls.left_side > THRESHOLD_LEFT_WALL
+	def too_left(self):
+		return self.force_y > THRESHOLD_LEFT_WALL
 
 	def run(self):
-		# wake up every 20Hz(50ms)and check lightsensor
+		# wake up every 20Hz(50ms)and check force sensor
 		rate = rospy.Rate(20)
 		data = Twist()
 
@@ -42,14 +69,14 @@ class WallAround():
 		data.angular.z = 0.0
 		
 		while not rospy.is_shutdown():
-			if self.wall_front(self.sensor_values):
+			if self.wall_front():
 				data.angular.z = - math.pi
-			elif self.too_right(self.sensor_values):
+			elif self.too_right():
 				data.angular.z = math.pi
-			elif self.too_left(self.sensor_values):
+			elif self.too_left():
 				data.angular.z = - math.pi
 			else:
-				error = DISTANCE_VALUE - self.sensor_values.left_side
+				error = DISTANCE_VALUE - self.force_z
 				data.angular.z = error * math.pi / 180.0
 			
 			# publish for travis test
